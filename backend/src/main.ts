d91d18c013012a612a7314467c96b03e2d95043d
Felix Prima <felix.prima@binus.ac.id>
2025-03-09 14:24:48 +0700

    feat: enable CORS and log server listening port in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
+
+  console.log("App is listening", process.env.PORT ?? 3000);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
