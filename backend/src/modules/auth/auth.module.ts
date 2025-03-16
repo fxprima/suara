f97484a76e975ec08c99b0493f046297f090960b
alyssaimani <alyssaimani44@gmail.com>
2025-03-16 22:53:53 +0700

    add jtwservice

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [UsersService, AuthService],
+  providers: [UsersService, AuthService, JwtService],
 })
 export class AuthModule {}
