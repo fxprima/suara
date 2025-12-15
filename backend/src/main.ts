1e2b65fa0f77f44168136e3c28b4ea091ba22a92
Felix <felix.prima@binus.ac.id>
2025-12-15 11:09:13 +0700

    chore: dockerize suara

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,21 +5,21 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   });
-
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true, // menolak data yang tidak didefinisikan dalam DTO
     transform: true, // mengubah tipe data yang dikirimkan sesuai dengan DTO
   }));
-
+  
   app.use(cookieParser());
   
-
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
